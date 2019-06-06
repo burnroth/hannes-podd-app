901f5c4d8371f342945e86cb4ca17cc83e6cffe7
burnroth <marcus,bernroth@lime.tech>
2019-06-06 16:23:43 +0200

    firebase-admin & firestore instead of firebase realtime db

--- first.js
+++ first.js
@@ -1,30 +1,55 @@
-var builder = require('xmlbuilder');
-var express = require('express');
-var router  = express.Router();
-var fs = require('fs');
+var builder = require("xmlbuilder");
+var express = require("express");
+var fs = require("fs");
 
-var dirPath = './hannes.xml'
+var router = express.Router();
+var dirPath = "./hannes.xml";
 
 
-var doc = builder.create('root')
-  .ele('xmlbuilder')
-    .att('for', 'node-js')
-    .ele('repo')
-      .att('type', 'git')
-      .txt('git://github.com/oozcitak/xmlbuilder-js.git') 
-      .up()
-    .up()
-  .ele('test')
-  .txt('complete')
-.end({ pretty: true });
+// FIREBASE
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+
+
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://maggan-b2fb6.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("kajak");
+
+
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+
+
+var doc = builder
+	.create("root")
+	.ele("xmlbuilder")
+	.att("for", "node-js")
+	.ele("repo")
+	.att("type", "git")
+	.att("maggan", "git")
+	.txt("git://github.com/oozcitak/xmlbuilder-js.git")
+	.up()
+	.up()
+	.ele("test")
+	.txt("complete")
+	.end({ pretty: true });
 
 var xmldoc = doc.toString({ pretty: true });
 
 fs.writeFile(dirPath, xmldoc, function(err) {
-  if(err) { return console.log(err); } 
-  console.log("The file was saved!");
-  res.render('index', { title: 'Generate XML using NodeJS' });
-
-}); 
-
-console.log(doc.toString());
+	if (err) {
+		return console.log(err);
+	}
+	console.log("The file was saved!");
+	// res.render("index", { title: "Generate XML using NodeJS" });
+});
