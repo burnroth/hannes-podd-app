42a48d29d22536c0ca7ead2573b6ec58b131252c
burnroth <marcus,bernroth@lime.tech>
2019-06-20 15:51:37 +0200

    realtime database version working almost as intended

--- first.js
+++ first.js
@@ -1,55 +1,64 @@
-var builder = require("xmlbuilder");
-var express = require("express");
-var fs = require("fs");
+const builder = require("xmlbuilder");
+const express = require("express");
+const fs = require("fs");
 
-var router = express.Router();
-var dirPath = "./hannes.xml";
-
+const router = express.Router();
+const dirPath = "./hannes.xml";
 
 // FIREBASE
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
-
-
-
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://maggan-b2fb6.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://maggan-b2fb6.firebaseio.com"
 });
 
-var db = admin.database();
-var ref = db.ref("kajak");
+const db = admin.database();
+const ref = db.ref("kajak");
 
+const doc = builder
+  .create("root")
+  .ele("xmlbuilder")
+  .att("for", "node-js")
+  .ele("repo")
+  .att("type", "git")
+  .att("maggan", "git")
+  .txt("git://github.com/oozcitak/xmlbuilder-js.git")
+  .up()
+  .up()
+  .ele("test")
+  .txt("complete")
+  .end({ pretty: true });
 
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-
-
-var doc = builder
-	.create("root")
-	.ele("xmlbuilder")
-	.att("for", "node-js")
-	.ele("repo")
-	.att("type", "git")
-	.att("maggan", "git")
-	.txt("git://github.com/oozcitak/xmlbuilder-js.git")
-	.up()
-	.up()
-	.ele("test")
-	.txt("complete")
-	.end({ pretty: true });
-
-var xmldoc = doc.toString({ pretty: true });
+const xmldoc = doc.toString({ pretty: true });
 
 fs.writeFile(dirPath, xmldoc, function(err) {
-	if (err) {
-		return console.log(err);
-	}
-	console.log("The file was saved!");
-	// res.render("index", { title: "Generate XML using NodeJS" });
+  if (err) {
+    return console.log(err);
+  }
+  console.log("The file was saved!");
+  // res.render("index", { title: "Generate XML using NodeJS" });
 });
+
+class Maggan {
+  constructor() {
+    this.number = "";
+	}
+
+  set number(val) {
+    ref.on(
+      "value",
+      function(snapshot) {
+				const value = snapshot.val();
+				this.number = value;
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      }
+    );
+	}
+	
+}
+
+const magan = new Maggan();
