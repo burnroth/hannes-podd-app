46819889ad933ce80e888d4525766ed771a220ab
burnroth <marcus,bernroth@lime.tech>
2019-08-24 13:04:50 +0200

    Managed to create the rss-file from the db. Having problem with the itunes-prefixes.

--- first.js
+++ first.js
@@ -1,58 +1,33 @@
+// Dependencies
 var builder = require("xmlbuilder");
-var express = require("express");
 var fs = require("fs");
-
-var router = express.Router();
-var dirPath = "./hannes.xml";
-
-
+var dirPath = "./hannes.rss";
 
 // FIREBASE
-
-var admin = require('firebase-admin');
-
+var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://maggan-b2fb6.firebaseio.com/"
 });
 
-var db = admin.firestore();
+var db = admin.database();
+var ref = db.ref("rss");
 
-db.collection('hejsan').doc('hej').get()
-.then(snap => { console.log(snap)})
-  .then((snapshot) => {
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });
+// Factory
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+
+  var obj = snapshot.val();
+  var doc = builder.create(obj).end({ pretty: true })
+  var xmldoc = doc.toString({ pretty: true });
+
+  fs.writeFile(dirPath, xmldoc, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
   })
-  .catch((err) => {
-    console.log('Error getting documents', err);
-  });
+})
 
-var rssData = {
-
-}
-
-var doc = builder
-	.create("rss").att("version","1.0").att("xmlns:itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd").att("xmlns:atom", "http://www.w3.org/2005/Atom")
-	.ele("xmlbuilder")
-	.att("for", "node-js")
-	.ele("repo")
-	.att("type", "git")
-	.att("maggan", "git")
-	.txt("git://github.com/oozcitak/xmlbuilder-js.git")
-	.up()
-	.up()
-	.ele("test")
-	.txt("complete")
-	.end({ pretty: true });
-
-var xmldoc = doc.toString({ pretty: true });
-
-fs.writeFile(dirPath, xmldoc, function(err) {
-	if (err) {
-		return console.log(err);
-	}
-	console.log("The file was saved!");
-	// res.render("index", { title: "Generate XML using NodeJS" });
-});
